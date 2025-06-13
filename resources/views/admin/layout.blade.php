2ca21a37d5ad2b1999e12fda02b40488d462210b
CleveeJ <willsoncj285@gmail.com>
2025-06-13 14:32:05 +0700

    Feat sidenav based on role

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -132,8 +132,9 @@
             <span class="cinzel text-center text-black font-bold">Admin Website<br>BGKL</span>
         </a>
         <ul class="relative m-0 list-none px-[0.2rem] pb-12" data-te-sidenav-menu-ref>
+            @if(in_array(session('role'), ['Manager', 'Supplier']))
             <li class="relative">
-
+                <span class="px-6 py-4 text-[0.6rem] font-bold uppercase text-black dark:text-gray-400">Supplier</span>
                 <!-- ISI ROUTE ALL PARTICIPANT -->
                 <a class="flex cursor-pointer items-center truncate rounded-[5px] px-6 py-[0.45rem] text-[0.85rem] text-black outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
                     data-te-sidenav-link-ref href="{{ route('admin.allSparepart') }}">
@@ -148,7 +149,11 @@
                     </span>
                     <span >All Sparepart</span>
                 </a>
-
+            </li>
+            @endif
+            @if(in_array(session('role'), ['Manager', 'Technician']))
+            <li class="relative">
+                <span class="px-6 py-4 text-[0.6rem] font-bold uppercase text-black dark:text-gray-400">Supplier</span>
                 <a class="flex cursor-pointer items-center truncate rounded-[5px] px-6 py-[0.45rem] text-[0.85rem] text-black outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
                     data-te-sidenav-link-ref href="{{ route('admin.serviceHistory') }}">
                     <span
@@ -163,6 +168,7 @@
                     <span >Service History</span>
                 </a>
             </li>
+            @endif
             <li class="relative pt-6">
                 <a href="{{ route('admin.logout') }}"
                     class="flex cursor-pointer items-center truncate rounded-[5px] px-6 py-[0.45rem] text-[0.85rem] text-black outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
